11d5ad6926ab0de8a02e45aa54d358c03c22bdbe
Zanidd <ninijay93@gmail.com>
2018-04-18 20:23:48 +0200

    added post

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const net = require('net');
 const dns = require('dns');
 const https = require("https");
+const querystring = require("querystring");
 
 var cmd = process.argv[2];
 var hostname = process.argv[3];
@@ -23,6 +24,42 @@ else if(cmd=="-g")
           body += data;
         });
       });
+}
+else if(cmd=="-p")
+{
+
+    var tasklist = {};
+    var postData = JSON.stringify({"tasks": [{"title":"Buy milk","done":false}]});
+
+    const options = {
+        hostname: hostname,
+        port: 443,
+        path: '/task_list/',
+        method: 'POST'
+      };
+
+      var stdin = process.openStdin();
+
+      console.log(postData);
+      const req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+      
+        res.on('data', (d) => {
+          process.stdout.write(d);
+        });
+      });
+      
+      req.on('error', (e) => {
+        console.error(e);
+      });
+      req.write(postData);
+      req.end();
+      
+}
+else
+{
+    console.log("usage");
 }
 
 
